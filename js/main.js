88b0d2d07239439838f6e5d18f4ef351ff5e515a
MR-SKYFALL <46224840+MR-SKYFALL@users.noreply.github.com>
2018-12-30 15:43:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
+let squaresX = 10;
+let squaresY = 8;
+let pointsArray = create2dArray(8,10);
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext('2d');
+let scale = 150;
+
 function Board(div_id) 
 {
     this.div = document.getElementById(div_id);
-    log(this.div);
-    this.canvas = document.createElement("canvas");
-    this.ctx = this.canvas.getContext('2d');
     this.canvasWidthResolution = 1800;
     this.canvasHeightResolution = 1200;
-    this.squaresX = 10;
-    this.squaresY = 8;
+    this.squaresX = squaresX;
+    this.squaresY = squaresY;
 
-    this.setup = function () 
+    this.draw = function () 
     {
-        this.div.appendChild(this.canvas);
-        this.canvas.width = this.canvasWidthResolution;
-        this.canvas.height = this.canvasHeightResolution;
-        this.ctx.moveTo(0,0);
-        this.ctx.lineTo(this.canvasWidthResolution,this.canvasHeightResolution);
-        this.ctx.stroke();
-
-        this.ctx.moveTo(0, this.canvasHeightResolution);
-        this.ctx.lineTo(this.canvasWidthResolution,0);
-        this.ctx.stroke();
-        
+        this.div.appendChild(canvas);
+        canvas.width = this.canvasWidthResolution;
+        canvas.height = this.canvasHeightResolution;        
     }
 }
 
@@ -31,17 +27,48 @@ function Point(x,y){
     this.moveTable = 
     [
         [0,0,0],
-        [0,0],
+        [0,2,0],
         [0,0,0]
     ];
+
+    this.draw=function(){
+        ctx.beginPath();
+            ctx.arc((this.x+1)*scale,this.y*scale,10,0,2*Math.PI,false);
+            ctx.fill();
+        ctx.closePath();
+    }
 }
 
-var myboard = new Board("board");
-myboard.setup();
+function setup(){
+    var myboard = new Board("board");
+    myboard.draw();
 
-//--------------------------------------------
-function log(x)
-{
-    if(true)
-        console.log(x);
+    for(let i=0; i<=squaresY; i++){
+        for(let j=0; j<=squaresX;j++){
+            pointsArray[i][j]=new Point(j,i);
+            if (i == 0) pointsArray[i][j].moveTable = [[2, 2, 2], [1, 2, 1], [0, 0, 0]];
+            if (j == 0) pointsArray[i][j].moveTable = [[2, 1, 0], [2, 2, 0], [2, 1, 0]];
+            if (i == squaresY) pointsArray[i][j].moveTable = [[0, 0, 0], [1, 2, 1], [2, 2, 2]];
+            if (j == squaresX) pointsArray[i][j].moveTable = [[0, 1, 2], [0, 2, 2], [0, 1, 2]];
+
+            pointsArray[i][j].draw();
+        }
+    }
+
+    ///ROGI///
+    pointsArray[0][0].moveTable = [[2, 2, 2], [2, 2, 1], [2, 1, 0]];
+    pointsArray[0][squaresX].moveTable = [[2, 2, 2], [1, 2, 2], [0, 1, 2]];
+    pointsArray[squaresY][squaresX].moveTable = [[0, 1, 2], [1, 2, 2], [2, 2, 2]];
+    pointsArray[squaresY][0].moveTable = [[2, 1, 0], [2, 2, 1], [2, 2, 2]];
+
+    ///BRAMKI///
+    pointsArray[((squaresY - 2) / 2) + 1][0].moveTable = [[2, 1, 0], [1, 2, 0], [0, 0, 0]];
+    pointsArray[((squaresY - 2) / 2) + 2][0].moveTable = [[0, 0, 0], [0, 2, 0], [0, 0, 0]];
+    pointsArray[((squaresY - 2) / 2) + 3][0].moveTable = [[0, 0, 0], [1, 2, 0], [2, 1, 0]];
+    pointsArray[((squaresY - 2) / 2) + 1][squaresX].moveTable = [[0, 1, 2], [0, 2, 1], [0, 0, 0]];
+    pointsArray[((squaresY - 2) / 2) + 2][squaresX].moveTable = [[0, 0, 0], [0, 2, 0], [0, 0, 0]];
+    pointsArray[((squaresY - 2) / 2) + 3][squaresX].moveTable = [[0, 0, 0], [0, 2, 1], [0, 1, 2]];
+
 }
+
+setup();
